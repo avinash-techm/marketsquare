33d3ed48f5727ad97923580ae407ba32ac9b8b0e
AS00465622 <AS00465622@INNOSZLP00716.TechMahindra.com>
2017-04-21 11:35:05 +0530

    update

--- WebContent/js/controller/userproducts-controller.js
+++ WebContent/js/controller/userproducts-controller.js
@@ -41,6 +41,38 @@ var productconfigCtrl = function($log, $scope, $interval, $location, ProductConf
 	
 	this.productsaccount3 = function()
 	{
+		
+		$scope.product.UploadProductName = $scope.UploadProductName;
+		$scope.product.UploadProductCode = $scope.UploadProductCode;
+		$scope.product.UploadProductTraits = $scope.UploadProductTraits;
+		$scope.product.UploadProductDescription = $scope.UploadProductDescription;
+		$scope.product.UploadProductunitmeasure = $scope.UploadProductunitmeasure;
+		$scope.product.UploadProductaskprice = $scope.UploadProductaskprice;
+		$scope.product.ProductDetailName = $scope.ProductDetailName;
+		$scope.product.ProductDetailSpeciallyTraits = $scope.ProductDetailSpeciallyTraits;
+		$scope.product.ProductDetailTraits = $scope.ProductDetailTraits;
+		$scope.product.ProductDetailServingSize = $scope.ProductDetailServingSize;
+		$scope.product.ProductDetailCalories = $scope.ProductDetailCalories;
+		$scope.product.ProductDetailTotalFat = $scope.ProductDetailTotalFat;
+		$scope.product.ProductDetailTotalFat = $scope.ProductDetailTotalFat;
+		$scope.product.ProductDetailTraitsFats = $scope.ProductDetailTraitsFats;
+		$scope.product.ProductDetailCholesterol = $scope.ProductDetailCholesterol;
+		$scope.product.ProductDetailSodium = $scope.ProductDetailSodium;
+		$scope.product.ProductDetailSugars = $scope.ProductDetailSugars;
+		$scope.product.PriceProductName = $scope.PriceProductName;
+		$scope.product.PriceUnitofMeasure = $scope.PriceUnitofMeasure;
+		$scope.product.PriceUnitSalePrice = $scope.PriceUnitSalePrice;
+		$scope.product.PriceBulkContractSales = $scope.PriceBulkContractSales;
+		$scope.product.PriceAskPrice = $scope.PriceAskPrice;
+		$scope.product.CertificationsProductName = $scope.CertificationsProductName;
+		$scope.product.CertificationsProductName = $scope.CertificationsProductNumber;
+		$scope.product.CertificationsDocument = $scope.CertificationsDocument;
+		$scope.product.CertificationsDocument1 = $scope.CertificationsDocument1;
+		$scope.product.CertificationsDocument2 = $scope.CertificationsDocument2;
+		$scope.product.CertificationsDocument3 = $scope.CertificationsDocument3;
+		$scope.product.CertificationsDocument4 = $scope.CertificationsDocument4;
+		$scope.product.CertificationsDocument5 = $scope.CertificationsDocument5;
+		
 		$scope.detailsyourproducts = false;
 		$scope.detailsyourproducts1 = false;
 		$scope.detailsyourproducts2 = false;
